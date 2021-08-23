0fa2f2f68fc857563570eba48dd16ea9ad4024d3
Siriluk Rachaniyom <6110613277@student.tu.ac.th>
2021-08-23 16:18:19 +0700

    Initial Commit

---
+++ Tic Tac Toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe
+//
+//  Created by Siriluk Rachaniyom on 23/8/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
